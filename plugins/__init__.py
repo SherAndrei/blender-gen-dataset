20623736057db2acbda415cf62bc46cf23cba138
SherAndrei <welldryus@gmail.com>
2025-05-05 00:39:09 +0300

    exclude patterns for masking objects

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -29,3 +29,35 @@ class IPlugin(object, metaclass=IPluginRegistry):
 
     def on_rendering_completed(self, scene):
         pass
+
+
+class RenderLayerToFileOutputMixin:
+    def get_render_layer_node(self, scene):
+        scene.use_nodes = True
+        scene.render.use_compositing = True
+
+        nodes = scene.node_tree.nodes
+        rl = nodes.get("Render Layers")
+        if not rl:
+            return nodes.new(type="CompositorNodeRLayers")
+        return rl
+
+    def create_file_output_node(self, scene, output_path, label, format_configuration):
+        nodes = scene.node_tree.nodes
+        file_out = nodes.new(type="CompositorNodeOutputFile")
+        file_out.label = label
+        file_out.base_path = output_path
+        file_out.format.file_format = format_configuration.get("file_format", "PNG")
+        file_out.format.color_mode = format_configuration.get("color_mode", "RGBA")
+        file_out.format.color_depth = format_configuration.get("color_depth", '8')
+        file_out.format.compression = format_configuration.get("compression", 15)
+
+        self._file_out = file_out
+
+        return file_out
+
+
+    def on_camera_created(self, scene, camera_obj, index, output_path):
+        node_output_file_slot_file = self._file_out.file_slots[0]
+        label = self._file_out.label
+        node_output_file_slot_file.path = f"{index:03d}_{label}_"
--- plugins/mask.py
+++ plugins/mask.py
@@ -1,45 +1,34 @@
-from plugins import IPlugin
+import plugins
 
-class Mask(IPlugin):
+class Mask(plugins.IPlugin, plugins.RenderLayerToFileOutputMixin):
     """For each camera view generate corresponding mask of the object.
        Resulting mask is saved in `output` directory with name 'AAA_mask_BBB.png',
        where AAA stands for index in the dataset and BBB stands for frame number.
     """
 
     def __init__(self, cfg, plugin_cfg):
-        self._rl = None
-        self._last_input_socket = None
-
+        self.plugin_cfg = plugin_cfg
 
     def on_scene_created(self, scene, output_path):
-        scene.use_nodes = True
-        scene.render.use_compositing = True
-        nodes = scene.node_tree.nodes
+        rl = self.get_render_layer_node(scene)
 
         view_layer = scene.view_layers[0]
         view_layer.use_pass_object_index = True
 
-        self._rl = nodes.get("Render Layers")
-        if not self._rl:
-            self._rl = nodes.new(type="CompositorNodeRLayers")
+        rl_index_output_socket = rl.outputs['IndexOB']
 
-        self._rl_index_output_socket = self._rl.outputs['IndexOB']
+        format_cfg = self.plugin_cfg.get("format", {})
+        file_out = self.create_file_output_node(scene, output_path, "mask", format_cfg)
+        scene.node_tree.links.new(rl_index_output_socket, file_out.inputs[0])
 
-        self._file_out = nodes.new(type="CompositorNodeOutputFile")
-        self._file_out.label = "MaskOutput"
-        self._file_out.base_path = output_path
-        self._file_out.format.file_format = 'PNG'
-
+        import re
+        exclude = self.plugin_cfg.get("exclude_pattern", "")
         for obj in scene.objects:
+            if re.search(obj.name, exclude):
+                continue
             if obj.type == 'MESH':
                 obj.pass_index = 1
 
 
     def on_camera_created(self, scene, camera_obj, index, output_path):
-        file_out = self._file_out
-        # avoid rewriting previous masks by removing the socket to them
-        if self._last_input_socket:
-            file_out.file_slots.remove(self._last_input_socket)
-
-        self._last_input_socket = file_out.file_slots.new(f"{index:03d}_mask_")
-        scene.node_tree.links.new(self._rl_index_output_socket, self._last_input_socket)
+        return plugins.RenderLayerToFileOutputMixin.on_camera_created(self, scene, camera_obj, index, output_path)
